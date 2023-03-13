fb2a70433a72eb295704548a7dfac3834ef25d4f
Nicolas Chiaravalle <116107079+Chiaravalle04@users.noreply.github.com>
2023-03-13 13:49:35 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="main-container">
+
+        <img src="{{ Vite::asset('resources/img/laravel.png') }}" alt="">
+        
+        <img src="{{ Vite::asset('resources/img/vite.png') }}" alt="">
+        
+        <img src="{{ Vite::asset('resources/img/sass.png') }}" alt="">
+        
+        <h1>Laravel - Vite - Sass | Template</h1>
+
+    </div>
+
+@endsection
