b376ac0c2e38bd83256251406ee5db90ff22e28c
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-03-14 10:57:09 +0100

    upgrade

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -11,13 +11,13 @@ use App\Models\Train;
 class PageController extends Controller
 {
     public function index() {
-
-        /* 
+        
+        /*
             MILESTONE 2
             
-            $trains = Train::where('departure_date', '2023-03-13')->get();
+            $trains = Train::where('departure_date', '>=', date('Y-m-d'))->get();
+        
         */
-
         $trains = Train::all();
 
         return view('welcome', compact('trains'));
